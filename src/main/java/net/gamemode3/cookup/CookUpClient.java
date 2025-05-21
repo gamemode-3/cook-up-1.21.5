ae57bdeb77d186f118a4fb787dda227765ebe4d6
gamemode-3 <simon@kriele.de>
2025-05-21 13:49:02 +0200

    initial commit

---
+++ src/main/java/net/gamemode3/cookup/CookUpClient.java
@@ -0,0 +1,10 @@
+package net.gamemode3.cookup;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CookUpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
